2c8ca87ee26271264ff29ec4ecda8d81240cea54
Lohit Devana <devana.lohit@gmail.com>
2019-03-08 14:45:45 -0500

    adding lambda changes

--- lambda.py
+++ lambda.py
@@ -1,20 +1,26 @@
+import json
 import boto3
 import StringIO
 import zipfile
-import mimetypes
 
-s3 = boto3.resource('s3')
-#/*assigning bucket variables*/
-resume_lohitdevana = s3.Bucket('resume.lohitdevana.info')
-build_lohitdevana = s3.Bucket('resumebuild.lohitdevana.info')
 
-#/*store the contents of the zip file in memory*/
-resume_zip = StringIO.StringIO()
-build_lohitdevana.download_fileobj('resumebuild.zip', resume_zip)
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    #/*assigning bucket variables*/
+    resume_lohitdevana = s3.Bucket('resume.lohitdevana.info')
+    build_lohitdevana = s3.Bucket('resumebuild.lohitdevana.info')
 
-#/*opens each file and upload them to the
-with zipfile.ZipFile(resume_zip) as myzip:
-    for nm in myzip.namelist():
-        obj=myzip.open(nm)
-        resume_lohitdevana.upload_fileobj(obj, nm)
-        resume_lohitdevana.Object(nm).Acl().put(ACL = 'public-read')
+    #/*store the contents of the zip file in memory*/
+    resume_zip = StringIO.StringIO()
+    build_lohitdevana.download_fileobj('resumebuild.zip', resume_zip)
+
+    #/*opens each file and upload them to the
+    with zipfile.ZipFile(resume_zip) as myzip:
+        for nm in myzip.namelist():
+            obj=myzip.open(nm)
+            resume_lohitdevana.upload_fileobj(obj, nm)
+            resume_lohitdevana.Object(nm).Acl().put(ACL = 'public-read')
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
