2dfbd7e5283bc1d1fc99ad7b735337427ec6d157
Lohit Devana <devana.lohit@gmail.com>
2019-03-10 11:23:37 -0400

    adding lambda to build file

--- lambda.py
+++ lambda.py
@@ -9,43 +9,27 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic ('arn:aws:sns:us-east-1:993318415470:deploynotification')
 
-    location = {
-        "bucketName":'resumebuild.lohitdevana.info',
-        "objectKey": 'resumebuild.zip'
-    }
 
-    try:
-        job = event.get("CodePipeline.job")
-
-        if job:
-            for articate in job["data"]["inputArtifacts"]:
-                if artifact["name"] == "BuildApp":
-                     location = artifact ["location"]["s3location"]
-
-        print "building the code " + str(location)
         #assigning  variables
-        resume_lohitdevana = s3.Bucket('resume.lohitdevana.info')
-        build_lohitdevana = s3.Bucket(location["bucketName"])
+    resume_lohitdevana = s3.Bucket('resume.lohitdevana.info')
+    build_lohitdevana = s3.Bucket('resumebuild.lohitdevana.info')
 
 
         #store the contents of the zip file in memory
-        resume_zip = StringIO.StringIO()
-        build_lohitdevana.download_fileobj(location["objectKey"], resume_zip)
+    resume_zip = StringIO.StringIO()
+    build_lohitdevana.download_fileobj('resumebuild.zip', resume_zip)
 
         #opens each file and upload them to the
-        with zipfile.ZipFile(resume_zip) as myzip:
-            for nm in myzip.namelist():
-                obj=myzip.open(nm)
-                resume_lohitdevana.upload_fileobj(obj, nm)
-                resume_lohitdevana.Object(nm).Acl().put(ACL = 'public-read')
+    with zipfile.ZipFile(resume_zip) as myzip:
+        for nm in myzip.namelist():
+            obj=myzip.open(nm)
+            resume_lohitdevana.upload_fileobj(obj, nm)
+            resume_lohitdevana.Object(nm).Acl().put(ACL = 'public-read')
         topic.publish(Subject="Deployment Notification- Success", Message="A new commit has been made and code is deployed succeffully")
-        if job:
-            codepipline = boto3.client('codepipline')
-            codepipline.put_job_success_result(jobId=job["id"])
-    except:
-        topic.publish(Subject="Deployment Notification- Failure", Message="A new commit has been made and code is deployment has failed")
-        raise
 
+    except:
+    topic.publish(Subject="Deployment Notification- Failure", Message="A new commit has been made and code is deployment has failed")
+    raise
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
